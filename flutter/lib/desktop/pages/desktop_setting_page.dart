69a02dbde2a24a10c043affbeb02829c70980e3d
FutureSpaces <113023234@QQ.COM>
2024-12-29 01:20:23 +0800

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -745,7 +745,8 @@ class _Safety extends StatefulWidget {
 class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
-  bool locked = bind.mainIsInstalled();
+  // bool locked = bind.mainIsInstalled();
+  bool locked = true;
   final scrollController = ScrollController();
 
   @override
@@ -756,7 +757,7 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
         child: Column(
           children: [
             _lock(locked, 'Unlock Security Settings', () {
-              locked = false;
+              locked = true;
               setState(() => {});
             }),
             preventMouseKeyBuilder(
