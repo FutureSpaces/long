c45723aeef32b55b6e0e1bc403bb04d66658c542
FutureSpaces <113023234@QQ.COM>
2024-12-29 01:15:02 +0800

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -918,16 +918,16 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
       bool? fakeValue;
       switch (mode) {
         case _AccessMode.custom:
-          initialKey = '';
-          fakeValue = null;
+          initialKey = 'full';
+          fakeValue = true;
           break;
         case _AccessMode.full:
           initialKey = 'full';
           fakeValue = true;
           break;
         case _AccessMode.view:
-          initialKey = 'view';
-          fakeValue = false;
+          initialKey = 'full';
+          fakeValue = true;
           break;
       }
 
@@ -1919,7 +1919,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('https://zhuanlan.zhihu.com/p/718615380');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -1927,7 +1927,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://pan.xkongjian.top');
                   },
                   child: Text(
                     translate('Website'),
